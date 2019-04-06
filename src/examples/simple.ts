4e089861d5baefeb5305931f0134204470c72a6e
James Abney <james.abney@gmail.com>
2019-04-06 13:26:47 -0700

    Add simple.ts to project

---
+++ src/examples/simple.ts
@@ -0,0 +1,23 @@
+/**
+ * Extract words and numbers
+ */
+import regexEach, { MatchCbk } from '..'
+
+const words: string[] = []
+const nums: number[] = []
+
+// Parse words and numbers.
+regexEach(/([a-z]+)|([0-9]+)/i, '10 apples for 5 dollars', (match) => {
+  const [, word, num] = match
+
+  if (word != null) {
+    words.push(word)
+  }
+
+  if (num != null) {
+    nums.push(parseInt(num, 10))
+  }
+})
+
+console.log('nums:', nums)
+console.log('words:', words)
